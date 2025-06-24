7ddcdda54f7011842d21132087beeb30d2efefa1
Maxerever <frosteyca67@gmail.com>
2025-06-25 00:33:27 +0300

    sfd

--- module/scripts/module.js
+++ module/scripts/module.js
@@ -557,7 +557,7 @@ async function Heal(actor) {
 async function Attack(currentDifficulty, actor, damage, currentWeapon) {
             if (!actor) return ui.notifications.warn("Выберите атакующего персонажа");
 
-            const target = Array.from(game.user.targets)[0].actor;
+            const target = Array.from(game.user.targets)[0].document.actor;
             if (!target) return ui.notifications.warn("Цель не выбрана");
 
             console.log("Цель — токен:", target.name, target.actorLink ? "link" : "unlinked");
